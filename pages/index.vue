74c76e5ee844abbca10aa27389180a9b9a69aff6
macoshita <macoshita@gmail.com>
2018-03-19 01:27:36 +0900

    Enhance top page.

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,21 @@
 main
   article
     header
-      h2 blog
+      h2 About
+    p Takahiro Yamakoshi (@macoshita)
+    ul
+      li: a(href="https://twitter.com/macoshita") Twitter
+      li: a(href="https://fb.com/takahiro.yamakoshi") Facebook
+      li: a(href="https://github.com/macoshita") GitHub
+  article
+    header
+      h2 Personal Projects
+    ul
+      li(v-for="pp in personalProjects", :key="pp.url")
+        a(:href="pp.url") {{ pp.year }} - {{ pp.name }}
+  article
+    header
+      h2 Blog
     ul
       li(v-for="post in posts", :key="post.slug")
         nuxt-link(:to="`/blog/${post.slug}/`") {{ post.date }} - {{ post.title }}
@@ -10,6 +24,17 @@ main
 
 <script>
 export default {
+  data () {
+    return {
+      personalProjects: [{
+        year: 2018, name: 'かなあそび', url: 'https://macoshita.github.io/play-kana-input'
+      }, {
+        year: 2017, name: 'AR りんごひろい for Pepper', url: 'https://youtu.be/dT5IBdxcRbg'
+      }, {
+        year: 2015, name: '折り返し翻訳', url: 'https://orikaeshi.com'
+      }]
+    }
+  },
   async asyncData ({ app, params }) {
     const blog = await import('blog-loader!~/blog.config.js')
     return {
